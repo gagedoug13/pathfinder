b8e88ac72591a199526465ef9562a06480039896
Gage Thornberry <gagedoug13@gmail.com>
2021-04-28 01:06:15 -0700

    added grid!

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -4,18 +4,31 @@ import Grid from './Grid'
 class GridContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            nodes: []
+        };
       }
     
     componentDidMount() {
-        
+        const nodes = []
+        for (let row=0; row<40; row++) {
+            nodes.push(0)
+            for (let col=0; col<32; col++) {
+                nodes.push(0)
+            }
+        }
+        this.setState({
+            nodes: nodes
+        })
     }
    render() {
-    return (
-        <div className='gridContainer'>
-            <Grid />
-        </div>
-    )
+    return this.state.nodes.map(x => {
+        return  (
+
+                <Grid nodes={this.state.nodes}/>
+            
+        )
+    })
    }
 
 }
