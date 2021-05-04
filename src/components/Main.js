c80cc6208c10ed299a5d843a60b83c3dc6e3d8ee
Gage Thornberry <gagedoug13@gmail.com>
2021-05-03 20:11:37 -0700

    deleted un-needed components and moved state to Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import FullGrid from './FullGrid'
-import Nav from './Nav'
-import NodeOptions from './NodeOptions'
+import React, { Component } from 'react'
+import Node from './Node'
+// import FullGrid from './FullGrid'
+// import Nav from './Nav'
+// import NodeOptions from './NodeOptions'
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: []
+        };
+      }
+    
+    componentDidMount() {
+        const nodes = []
+        for (let row=0; row<30; row++) {
+            const currentRow = []
+            for (let col=0; col<20; col++) {
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col === 45,
 
-export default function Main(props) {
-    return (
-        <div className='main'>
-            <Nav />
-            <NodeOptions />
-            <FullGrid />
-        </div>
-    )
+                }
+                currentRow.push(currentNode)
+            }
+            nodes.push(currentRow)
+        }
+        this.setState({
+            nodes: nodes
+        })
+    }
+    counter = 0
+   render() {
+       const nodes = this.state.nodes
+        return  (
+            <div className='grid'>
+                {nodes.map((row, rowId) => {
+                    return (
+                        <div className='nodesContainer'>
+                            {row.map((node, nodeId) => {
+                                return (
+                                    <Node />
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+                <Node counter={this.counter} nodes={this.state.nodes}/>
+            </div>
+        )
+   }
+
 }
+
+export default Main;
---
+++ src/components/Node.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Nodes(props) {
+    return (
+        <div id={`block${props.counter}`} className='node'>
+        </div>
+    )
+}
